ec4793b26135621a0a250c7cbf338629cc625e36
insung choi <choiis1207@gmail.com>
2022-02-06 09:49:12 +0900

    20220206 rds 생성 추가

---
+++ database.tf
@@ -0,0 +1,15 @@
+resource "aws_db_subnet_group" "db_subnet" {
+  name       = "db_subnet_group"
+  subnet_ids = [aws_subnet.public_subnet_1a.id,aws_subnet.public_subnet_1b.id]
+}
+resource "aws_db_instance" "mysql_rds" {
+  allocated_storage = 8
+  engine = "mysql"
+  engine_version = "5.7.36"
+  instance_class = "db.t2.micro"
+  name = "mysql_rds"
+  username = "users"
+  password = "12345678"
+  db_subnet_group_name = "db_subnet"
+  vpc_security_group_ids = [aws_security_group.terra_security.id]
+}
--- security.tf
+++ security.tf
@@ -10,3 +10,16 @@ resource "aws_security_group" "terra_security" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_security_group" "db_security" {
+    name        = "db_security"
+    description = "db security group by terraform"
+    vpc_id      = aws_vpc.vpc.id
+
+    ingress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = ["180.71.20.47/32"]
+    }
+}
